221f56fe8b528f3df97e4ccfa6e7e77f23a468f5
Sajjat Hossain <Sajjad Sadiq>
2022-11-24 07:09:43 +0600

    react icons and react router dom install

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { FaBeer } from 'react-icons/fa';
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!<FaBeer /></h1>;
 };
 
 export default App;
